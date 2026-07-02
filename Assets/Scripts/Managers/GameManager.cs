e0aae122425414d6b4c9277486be4ebfd8494784
Mohammmed Nizam k n <mohammednizamkn217@gmail.com>
2026-07-02 18:42:43 +0400

    Phase 7 - Implement player turn system

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,13 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    [SerializeField] private Board board;
+    [SerializeField] private UIManager uiManager;
+
+    private PlayerType currentPlayer = PlayerType.X;
+
+    private PlayerType?[] boardState = new PlayerType?[9];
+
     private void Awake()
     {
         if (Instance == null)
@@ -12,8 +19,31 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void Start()
+    {
+        uiManager.UpdateTurnText(currentPlayer);
+    }
+
     public void SelectCell(int index)
     {
-        Debug.Log("Cell Clicked : " + index);
+        if (boardState[index] != null)
+            return;
+
+        boardState[index] = currentPlayer;
+
+        string symbol = currentPlayer == PlayerType.X ? "X" : "O";
+
+        board.Cells[index].SetSymbol(symbol);
+
+        SwitchTurn();
+    }
+
+    private void SwitchTurn()
+    {
+        currentPlayer = currentPlayer == PlayerType.X
+            ? PlayerType.O
+            : PlayerType.X;
+
+        uiManager.UpdateTurnText(currentPlayer);
     }
 }
