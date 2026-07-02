598af7ea610b5c669ffde6fbc78fc81a30dcd26f
Mohammmed Nizam k n <mohammednizamkn217@gmail.com>
2026-07-02 19:15:27 +0400

    Phase 10 - Scene management and navigation

---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
